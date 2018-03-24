9238b324361294cebca59eede8258a1f2d792023
Dmytro P <37566861+dpsrc@users.noreply.github.com>
2018-03-24 00:36:43 -0600

    Update unlimitedspanish_dnld_podcasts.pl

--- unlimitedspanish_dnld_podcasts.pl
+++ unlimitedspanish_dnld_podcasts.pl
@@ -4,8 +4,8 @@
 # the excellent Spanish learning resource by Òscar Pellus I have been lucky to enjoy.
 # It parses each episode page and obtains the URLs of .mp3 and .pdf files as well as the episode name.
 # Then it downloads these files and renames them to the episode name.
-# Different formats of URLs and names for different episode diapasons as well as known special cases (typos and not only those)
-# for some episodes are taken into account.
+# Different formats of URLs and names for different episode diapasons as well as known special cases (typos,
+# non-standard naming convention, etc.) for some episodes are taken into account.
 #
 # **TODO: remove debug information and make it possible to take episode numbers and diapasons from the command line
 # and check if there are known special cases for episodes after 80.**
