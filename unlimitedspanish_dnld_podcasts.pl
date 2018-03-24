b6b4fcf1edbf6f2d184939e0968b8016c32cac2e
Dmytro P <37566861+dpsrc@users.noreply.github.com>
2018-03-24 00:35:47 -0600

    Update unlimitedspanish_dnld_podcasts.pl

--- unlimitedspanish_dnld_podcasts.pl
+++ unlimitedspanish_dnld_podcasts.pl
@@ -1,14 +1,17 @@
 #! /usr/bin/perl -w
 
-# Downloads the podcast episodes from https://www.unlimitedspanish.com/podcasts/ -
-# the excellent Spanish learning resource.
-# It parses each episode page and obtains the URLs of .mp3 and .pdf files
-# as well as the episode name.
+# Downloads the podcast episodes from https://www.unlimitedspanish.com/podcasts/ - 
+# the excellent Spanish learning resource by Òscar Pellus I have been lucky to enjoy.
+# It parses each episode page and obtains the URLs of .mp3 and .pdf files as well as the episode name.
 # Then it downloads these files and renames them to the episode name.
-# Different formats of URLs and names for different episode diapasons
-# as well as known special cases (typos and not only those) for some episodes
-# are taken into account.
-# TODO: remove debug information and make it possible to take episode numbers and diapasons from the command line and check if there are known special cases for episodes after 80.
+# Different formats of URLs and names for different episode diapasons as well as known special cases (typos and not only those)
+# for some episodes are taken into account.
+#
+# **TODO: remove debug information and make it possible to take episode numbers and diapasons from the command line
+# and check if there are known special cases for episodes after 80.**
+#
+# **TODO: Implement downloading episodes from http://language-efficiency.com/unlimited-spanish-podcast/ -
+# audio, video, and text of original episodes translated into Russian and excellently explained in Russian by Dmitry Gurbatov.**
 
 my $iform; # Formatted episode number, e.g., 4 -> 004, 12 -> 012, 104 -> 104
 my $urlpage; # Web page URL with an eposide
